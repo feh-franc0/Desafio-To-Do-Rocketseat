40f8302d0f6716a1b8c009d0747ffab66d4f9023
feh-franc0 <fernandofv1110@gmail.com>
2022-11-25 21:50:45 -0300

    Componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
-function App() {
+import {Post} from './Post';
+
+export function App() {
   return (
-    <h1>Hello World!</h1>
+    <>
+      <h1>Info:</h1>
+      <Post />
+      <Post />
+    </>
   )
 }
-
-export default App
---
+++ src/Post.jsx
@@ -0,0 +1,8 @@
+export function Post() {
+  return (
+    <div>
+      <p>Nome: Felipe Alexandre</p>
+      <p>idade: 50</p>
+    </div>
+  )
+}
