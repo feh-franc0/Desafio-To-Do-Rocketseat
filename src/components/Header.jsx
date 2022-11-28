2bbab7ae501775170fa5681ffd4b3ad85555eaaa
feh-franc0 <fernandofv1110@gmail.com>
2022-11-28 14:59:46 -0300

    componente: Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,6 @@
-import logo from './Logo.svg'
+import logo from '../assets/Logo.svg'
 import styles from './Header.module.css'
-console.log(styles); //* vem um objeto com o nome da classe mt estranha rsrsrs!
-console.log(logo)
+
 export function Header() {
   return (
     <header className={styles.header}>
