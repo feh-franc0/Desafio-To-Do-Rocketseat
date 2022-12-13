03efed7418b2a2f157499dd267a079af03602871
feh-franc0 <fernandofv1110@gmail.com>
2022-12-12 23:02:26 -0300

    adicionando formulario para adicionar tasks

--- src/components/NewTask.jsx
+++ src/components/NewTask.jsx
@@ -3,12 +3,13 @@ import iconAddTask from '../assets/iconAdd.svg'
 
 export function NewTask() {
   return (
-    <div className={styles.containerTask}>
-      <input className={styles.inputTask} type="text" /*value='Adicione uma nova tarefa'*/ />
-      <button className={styles.buttonTask}>
-        <span>Criar</span>
-        <img src={iconAddTask} alt="add icon" />
-      </button>
-    </div>
+      <form action="" className={styles.containerTask}>
+        <textarea className={styles.inputTask} type="text" placeholder="Deixe um comentário" />
+
+        <button className={styles.buttonTask} type="submit">
+          <span>Criar</span>
+          <img src={iconAddTask} alt="add icon" />
+        </button>
+      </form>
   );
 }
